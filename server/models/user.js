f89e4d664664191d42606652efad6151614dbef3
william967945 <william967945@yahoo.com>
2022-07-19 18:10:32 +0800

    first commit

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  // email: { type: String, required: true },
-  // password: { type: String, required: true },
-  // id: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  id: { type: String },
 });
 
 const User = mongoose.model('User', userSchema);
